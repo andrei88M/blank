b8007340e396f039a5f23c87cd86825750e0ef29
andrei88M <andrei1988mikhalchuk@gmail.com>
2022-06-09 16:37:18 +0300

    update2

--- src/main/java/com/example/test/entity/User.java
+++ src/main/java/com/example/test/entity/User.java
@@ -3,8 +3,6 @@ package com.example.test.entity;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -27,26 +25,36 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return (Collection<? extends GrantedAuthority>) getRoles();
+        return  getRoles();
     }
 
+    //Метод isAccountNonExpired возвращает логический тип, который используется для определения того,
+    //    не истек ли срок действия учетной записи, если он не истек, он возвращает true, в противном случае возвращает false;
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    //Метод isAccountNonLocked используется для определения, разблокирована ли учетная запись;
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    //isCredentialsNonExpired используется для определения того, не истек ли срок действия учетных данных пользователя,
+    //то есть не истек ли срок действия пароля;
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    //Метод isEnabled используется для определения доступности пользователя.
+    //На практике мы можем настроить класс реализации интерфейса UserDetails или напрямую использовать
+    //класс реализации интерфейса UserDetails org.springframework.security.core.userdetails.User,
+    // предоставленный Spring Security.
     @Override
     public boolean isEnabled() {
         return true;
     }
+
 }
